45c8d2e1cd4096ce0d3a0ff927783c2d30bbef1d
Iurii <juravyn@gmail.com>
2021-05-27 21:05:53 +0300

    Code refactoring

--- DemoblazeAutoTest.HomeTask1/LoginTest.cs
+++ DemoblazeAutoTest.HomeTask1/LoginTest.cs
@@ -4,13 +4,15 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+using System.IO;
 
 namespace DemoblazeAutoTest.HomeTask1
 {
     [TestFixture]
     public class LoginTest
     {
-        IWebDriver driverChrome;
+        IWebDriver driver;
+        string testUrl;
         WebDriverWait wait;
         string userName;
         string userPassword;
@@ -18,58 +20,55 @@ namespace DemoblazeAutoTest.HomeTask1
         [OneTimeSetUp]
         public void Setup()
         {
-            driverChrome = new ChromeDriver();
-            driverChrome.Manage().Window.Maximize();
-            wait = new WebDriverWait(driverChrome, TimeSpan.FromSeconds(5));
-            userName = "userTest001";
-            userPassword = "testPassword";
+            driver = new ChromeDriver(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void VerifyValidLogin()
         {
-            driverChrome.Url = "https://www.demoblaze.com/index.html";
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            testUrl = "https://www.demoblaze.com/index.html";
+            userName = "userTest001";
+            userPassword = "testPassword";
 
-            IWebElement buttonLogin =  driverChrome.FindElement(By.Id("login2"));
+            driver.Navigate().GoToUrl(testUrl);
+
+            IWebElement buttonLogin =  driver.FindElement(By.Id("login2"));
             //other options: 
                 // xPath       =  //ul//li//a[@data-target='#logInModal'] or //a[text()='Log in']
                 // cssSelector =  #login2 or a[data-target='#logInModal']
             buttonLogin.Click();
 
-            IWebElement modalWindowLogin = wait.Until(e => e.FindElement(By.Id("logInModal")));
+            wait.Until(drv => drv.FindElement(By.Id("logInModal")));
             //other options: 
                 // xPath       =  //div[@aria-labelledby="logInModalLabel"]
                 // cssSelector =  #login2 or a[data-target='#logInModal']
             
-            IWebElement inputUsername = driverChrome.FindElement(By.Id("loginusername"));
+            IWebElement inputUsername = driver.FindElement(By.Id("loginusername"));
             //other options:
                 //xPath (complex) = //form//div[@class='form-group']//label[@for='log-name']/following-sibling::input
                 //cssSelector     = input[id="loginusername"]
             inputUsername.Clear();
             inputUsername.SendKeys(userName);
             
-            IWebElement inputPassword = driverChrome.FindElement(By.Id("loginpassword"));
+            IWebElement inputPassword = driver.FindElement(By.Id("loginpassword"));
             //other options:
                 //xPath (complex) = //label[@for="log-pass"]/parent::div/child::input
                 //cssSelector     = input[id='sign-password']
             inputPassword.Clear();
             inputPassword.SendKeys(userPassword);
 
-            IWebElement buttonLoginSubmit = driverChrome.FindElement(By.CssSelector("button.btn.btn-primary[onclick='logIn()']"));
+            IWebElement buttonLoginSubmit = driver.FindElement(By.CssSelector("button.btn.btn-primary[onclick='logIn()']"));
             //other option:
                 //XPath = //div[@class='modal-footer']//button[@onclick='logIn()']
             buttonLoginSubmit.Click();
-
-            IWebElement logoutButton = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("logout2")));
-            //other options:
-                //xPath (complex) = //a[@onclick="logOut()"]
-            //cssSelector     = a[id$='out2']
 
             IWebElement welcomeUsername = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("nameofuser")));
 
             Assert.Multiple(() =>
             {
-                Assert.That(logoutButton.Displayed, Is.True, "Logout is not displayed");
+                Assert.That(driver.FindElement(By.Id("logout2")).Displayed, Is.True, "Logout is not displayed");
                 Assert.That(welcomeUsername.Text, Is.EqualTo("Welcome " + userName), "Actual username differs from expected");
             });
         }
@@ -77,7 +76,7 @@ namespace DemoblazeAutoTest.HomeTask1
         [OneTimeTearDown]
         public void Cleanup()
         {
-            driverChrome.Quit();
+            driver.Quit();
         }
     }
 }
