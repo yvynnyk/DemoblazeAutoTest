630df4701cf5fba993303d63b6b884313bcaff19
Iurii <juravyn@gmail.com>
2021-06-05 20:12:36 +0300

    Uncomment XPath and CssSelectors options for WebElements; assign them to varialbles accordingly;

--- DemoblazeAutoTest.HomeTask1/LoginTest.cs
+++ DemoblazeAutoTest.HomeTask1/LoginTest.cs
@@ -22,46 +22,50 @@ namespace DemoblazeAutoTest.HomeTask1
         {
             driver = new ChromeDriver(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
             driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            testUrl = "https://www.demoblaze.com/index.html";
+            userName = "userTest001";
+            userPassword = "testPassword";
         }
 
         [Test]
         public void VerifyValidLogin()
         {
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            testUrl = "https://www.demoblaze.com/index.html";
-            userName = "userTest001";
-            userPassword = "testPassword";
-
             driver.Navigate().GoToUrl(testUrl);
 
             IWebElement buttonLogin =  driver.FindElement(By.Id("login2"));
             //other options: 
-                // xPath       =  //ul//li//a[@data-target='#logInModal'] or //a[text()='Log in']
-                // cssSelector =  #login2 or a[data-target='#logInModal']
+            buttonLogin = driver.FindElement(By.XPath("//ul//li//a[@data-target='#logInModal']"));
+            buttonLogin = driver.FindElement(By.XPath("//a[text()='Log in']"));
+            buttonLogin = driver.FindElement(By.CssSelector("#login2"));
+            buttonLogin = driver.FindElement(By.CssSelector("a[data-target='#logInModal']"));
+
             buttonLogin.Click();
 
             wait.Until(drv => drv.FindElement(By.Id("logInModal")));
-            //other options: 
-                // xPath       =  //div[@aria-labelledby="logInModalLabel"]
-                // cssSelector =  #login2 or a[data-target='#logInModal']
-            
+            //other option: 
+            wait.Until(drv => drv.FindElement(By.XPath("//div[@aria-labelledby='logInModalLabel']")));
+
             IWebElement inputUsername = driver.FindElement(By.Id("loginusername"));
             //other options:
-                //xPath (complex) = //form//div[@class='form-group']//label[@for='log-name']/following-sibling::input
-                //cssSelector     = input[id="loginusername"]
+            inputUsername = driver.FindElement(By.XPath("//form//div[@class='form-group']//label[@for='log-name']/following-sibling::input"));
+            inputUsername = driver.FindElement(By.CssSelector("input[id='loginusername']"));
+            
             inputUsername.Clear();
             inputUsername.SendKeys(userName);
             
             IWebElement inputPassword = driver.FindElement(By.Id("loginpassword"));
             //other options:
-                //xPath (complex) = //label[@for="log-pass"]/parent::div/child::input
-                //cssSelector     = input[id='sign-password']
+            inputPassword = driver.FindElement(By.XPath("//label[@for='log-pass']/parent::div/child::input"));
+            inputPassword = driver.FindElement(By.CssSelector("input[id='loginpassword']"));
+            
             inputPassword.Clear();
             inputPassword.SendKeys(userPassword);
 
             IWebElement buttonLoginSubmit = driver.FindElement(By.CssSelector("button.btn.btn-primary[onclick='logIn()']"));
             //other option:
-                //XPath = //div[@class='modal-footer']//button[@onclick='logIn()']
+            buttonLoginSubmit = driver.FindElement(By.XPath("//div[@class='modal-footer']//button[@onclick='logIn()']"));
+            
             buttonLoginSubmit.Click();
 
             IWebElement welcomeUsername = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("nameofuser")));
